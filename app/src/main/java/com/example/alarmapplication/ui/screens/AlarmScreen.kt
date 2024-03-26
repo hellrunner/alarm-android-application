3a1f6b3de223abb8150631b96ddbd73f77c785de
NikitaAlekseevich <tnikitaalekseevich@gmail.com>
2024-03-26 16:34:41 +0300

    Added screen layout with alarm clock

--- app/src/main/java/com/example/alarmapplication/ui/screens/AlarmScreen.kt
+++ app/src/main/java/com/example/alarmapplication/ui/screens/AlarmScreen.kt
@@ -1,21 +1,62 @@
 package com.example.alarmapplication.ui.screens
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontFamily
-
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun AlarmScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Тут Будет Будильник")
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .align(Alignment.Center),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Будильников нет",
+                fontSize = 20.sp
+            )
+        }
+        Row(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(16.dp)
+        ) {
+            FloatingActionButton(
+                onClick = {}, // Обработчик событий для первой кнопки
+                modifier = Modifier.padding(end = 8.dp),
+                containerColor = MaterialTheme.colorScheme.primary
+            ) {
+                Icon(Icons.Filled.Edit, contentDescription = "Edit")
+            }
+            FloatingActionButton(
+                onClick = {}, // Обработчик событий для второй кнопки
+                containerColor = MaterialTheme.colorScheme.primary
+            ) {
+                Icon(Icons.Filled.Add, contentDescription = "Add")
+            }
+        }
     }
 }
+
+
+
+//@Composable
+//fun AlarmScreen() {
+//    Box(
+//        modifier = Modifier
+//            .fillMaxSize(),
+//        contentAlignment = Alignment.Center
+//    ) {
+//        Text(text = "Тут Будет Будильник")
+//    }
+//}
