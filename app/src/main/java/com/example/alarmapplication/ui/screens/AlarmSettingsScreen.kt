d80c7798f296f8afbcff8a394e5638c7e39b4fa2
DarkDragon3333 <minaichiev-dragon2017@yandex.ru>
2024-04-12 19:33:00 +0300

    Доделать TimePicker

---
+++ app/src/main/java/com/example/alarmapplication/ui/screens/AlarmSettingsScreen.kt
@@ -0,0 +1,12 @@
+package com.example.alarmapplication.ui.screens
+
+import android.app.AlarmManager
+import android.content.BroadcastReceiver
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun AlarmSettings(){
+
+}
--- app/src/main/java/com/example/alarmapplication/ui/screens/MusicScreen.kt
+++ app/src/main/java/com/example/alarmapplication/ui/screens/MusicScreen.kt
@@ -1,19 +1,112 @@
 package com.example.alarmapplication.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TimePickerColors
+import androidx.compose.material3.TimePickerDefaults
+import androidx.compose.material3.TimePickerLayoutType
+import androidx.compose.material3.TimePickerState
+import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 
 @Composable
 fun MusicScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
     ) {
-        Text(text = "Тут Будут Мелодии")
+        Text(
+            fontWeight = FontWeight.Bold,
+            fontSize = 28.sp,
+            text = "Выберете мелодию"
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(0.8f),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .size(120.dp, 80.dp),
+                shape = RoundedCornerShape(8.dp),
+            ) {
+
+            }
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.size(120.dp, 80.dp),
+                shape = RoundedCornerShape(8.dp)
+            ) {
+            }
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(0.8f),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .size(120.dp, 80.dp),
+                shape = RoundedCornerShape(8.dp)
+            ) {
+
+            }
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.size(120.dp, 80.dp),
+                shape = RoundedCornerShape(8.dp)
+            ) {
+
+            }
+        }
+        TimePickerView()
+
     }
+}
+
+@Composable
+@ExperimentalMaterial3Api
+fun TimePicker(
+    state: TimePickerState,
+    modifier: Modifier = Modifier,
+    colors: TimePickerColors = TimePickerDefaults.colors(),
+    layoutType: TimePickerLayoutType = TimePickerDefaults.layoutType(),
+) {
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun TimePickerView(){
+    val timeState = rememberTimePickerState(11, 30, false)
+    TimePicker(timeState)
 }
