668b9d2ff5fbf93b9722f1d2c4a6a502bead8158
DarkDragon3333 <minaichiev-dragon2017@yandex.ru>
2024-04-12 15:28:17 +0300

    Alarm UI 1.0

--- app/src/main/java/com/example/alarmapplication/ui/screens/AlarmScreen.kt
+++ app/src/main/java/com/example/alarmapplication/ui/screens/AlarmScreen.kt
@@ -1,21 +1,104 @@
 package com.example.alarmapplication.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.FloatingActionButtonElevation
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.alarmapplication.model.Alarm
+import com.example.alarmapplication.ui.components.AlarmItem
 
-
+@Preview
 @Composable
 fun AlarmScreen() {
+    val alarms = listOf(
+        Alarm(
+            "5:50",
+            "Вт",
+            true
+        ),
+        Alarm(
+            "5:50",
+            "Вт",
+            true
+        ),
+        Alarm(
+            "5:50",
+            "Вт",
+            true
+        ),
+        Alarm(
+            "5:50",
+            "Вт",
+            true
+        ),
+        Alarm(
+            "5:50",
+            "Вт",
+            true
+        ),
+        Alarm(
+            "5:50",
+            "Вт",
+            true
+        ),
+        Alarm(
+            "5:50",
+            "Вт",
+            true
+        ),
+        Alarm(
+            "5:50",
+            "Вт",
+            true
+        ),
+        Alarm(
+            "5:50",
+            "Вт",
+            true
+        ),
+
+        )
+
+
+    LazyColumn(
+        verticalArrangement = Arrangement.Center,
+    ) {
+        items(alarms) { alarm ->
+            AlarmItem(alarm = alarm)
+        }
+    }
+
+
     Box(
         modifier = Modifier
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
+            .fillMaxSize()
+            .padding(8.dp),
+        Alignment.BottomCenter
     ) {
-        Text(text = "Тут Будет Будильник")
+        FloatingActionButton(
+            onClick = { /*TODO*/ },
+            elevation = FloatingActionButtonDefaults.elevation(defaultElevation = 1.dp)
+        ) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "Add by")
+        }
     }
+
+
 }
